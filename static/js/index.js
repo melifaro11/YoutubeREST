6703d8321783ec834fc7a93861cf17843bf1d675
miaraylight <miaraylight@gmail.com>
2023-04-12 20:54:21 +0200

    changed background position to 39 degrees and fix some css properties, renamed classes

--- static/js/index.js
+++ static/js/index.js
@@ -1,85 +1,89 @@
-const form = document.querySelector('.form')
-const dwnlForm = document.querySelector('.dwnlForm')
-const progress = document.querySelector('.progress')
-
-
+const form = document.querySelector(".form");
+const cardWrapper = document.querySelector(".card-wrapper");
+const progress = document.querySelector(".progress");
 
 function byteToStr(bytes) {
-    if (bytes < 1024) {
-        return bytes + " bytes"
-    } else if (bytes < 1024 * 1024) {
-        return (bytes / 1024).toFixed(2) + " KB";
-    } else if (bytes < 1024 * 1024 * 1024) {
-        return (bytes / 1024 / 1024).toFixed(2) + " MB";
-    }
- }
+  if (bytes < 1024) {
+    return bytes + " bytes";
+  } else if (bytes < 1024 * 1024) {
+    return (bytes / 1024).toFixed(2) + " KB";
+  } else if (bytes < 1024 * 1024 * 1024) {
+    return (bytes / 1024 / 1024).toFixed(2) + " MB";
+  }
+}
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const targetURL = e.target.url.value;
-    progress.style.display = 'block'
-    let response = await fetch('/streams', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(targetURL)
-    }).then(response => response.json())
-    .then(json => {
-        if ('error' in json) {
-            console.log('ERROR: ' + json['error']);
-        } else {
-            json['streams'] = json['streams'].filter(({progressive}) => progressive)
-            createControls(json)
-        }
-    })
-    e.target.url.value = ""
-    })
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
+  const targetURL = e.target.url.value;
+
+  progress.style.display = "block";
+
+  let response = await fetch("/streams", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(targetURL),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if ("error" in data) {
+        console.log("ERROR: " + data["error"]);
+      } else {
+        data["streams"] = data["streams"].filter(({ progressive }) => progressive);
+        createControls(data);
+      }
+    });
+
+  e.target.url.value = "";
+});
 
 function createControls(data) {
-    progress.style.display = 'none'
-    const title = document.createElement('p')
-    const dwnlBtn = document.createElement('button')
-    const select = document.createElement('select')
-    const thumbnail = document.createElement('img')
-    const wrapper = document.createElement('div')
-    const btnImg = document.createElement('img')
+  progress.style.display = "none";
+  cardWrapper.style.display = "grid";
 
-    const streams = data['streams']
-    
-    for (let i = 0; i < streams.length; i++) {
-        const option = document.createElement('option')
-        const optionValue = [byteToStr(streams[i]['file_size']) , streams[i]['res']]
-        option.value = streams[i]['itag']
-        option.innerText = optionValue.join('/')
+  const title = document.createElement("p");
+  const dwnlBtn = document.createElement("button");
+  const select = document.createElement("select");
+  const thumbnail = document.createElement("img");
+  const wrapper = document.createElement("div");
+  const btnImg = document.createElement("img");
 
-        select.append(option)
-    }
+  const streams = data["streams"];
 
-    title.innerHTML = data['title']
-    thumbnail.src = data['thumbnail']
-    btnImg.src = '/images/icon-download.png'
+  for (let i = 0; i < streams.length; i++) {
+    const option = document.createElement("option");
+    option.value = streams[i]["itag"];
 
-    dwnlBtn.classList.add('downloadBtn')
-    title.classList.add('title')
-    select.classList.add('select')
+    const optionText = [byteToStr(streams[i]["file_size"]), streams[i]["res"]];
+    option.innerText = optionText.join("/");
 
-    dwnlBtn.addEventListener('click', () => {
-        const itag = select.value
-        const link = document.createElement("a");
-        link.href = `/download/${itag}`
-        link.click()
-        rerender()
-    })
+    select.append(option);
+  }
 
-    dwnlBtn.append(btnImg)
-    wrapper.append(select, dwnlBtn)
-    dwnlForm.append( thumbnail, title, wrapper)
- 
- }
- 
- function rerender() {
-    dwnlForm.innerText = ""
- }
+  title.innerHTML = data["title"];
+  thumbnail.src = data["thumbnail"];
+  btnImg.src = "/images/icon-download.png";
 
+  dwnlBtn.classList.add("downloadBtn");
+  title.classList.add("title");
+  select.classList.add("select");
+
+  dwnlBtn.addEventListener("click", () => {
+    const itag = select.value;
+    const link = document.createElement("a");
+    link.href = `/download/${itag}`;
+    link.click();
+    rerender();
+  });
+
+  dwnlBtn.append(btnImg);
+  wrapper.append(select, dwnlBtn);
+  cardWrapper.append(thumbnail, title, wrapper);
+}
+
+function rerender() {
+  cardWrapper.innerText = "";
+  cardWrapper.style.display = "none";
+}
